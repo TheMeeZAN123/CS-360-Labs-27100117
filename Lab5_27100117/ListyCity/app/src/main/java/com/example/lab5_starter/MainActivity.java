98d8442f659b44cb62dae94661aff1ef07fae3b0
Ali Raza <killerkip4554@gmail.com>
2026-02-26 09:12:49 +0500

    lab 5

---
+++ Lab5_27100117/ListyCity/app/src/main/java/com/example/lab5_starter/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.lab5_starter;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CityDialogFragment.CityDialogListener {
+
+    private Button addCityButton;
+    private ListView cityListView;
+
+    private ArrayList<City> cityArrayList;
+    private ArrayAdapter<City> cityArrayAdapter;
+    private FirebaseFirestore db;
+    private CollectionReference citiesRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        db = FirebaseFirestore.getInstance();
+        citiesRef = db.collection("cities");
+
+        // Set views
+        addCityButton = findViewById(R.id.buttonAddCity);
+        cityListView = findViewById(R.id.listviewCities);
+
+        // create city array
+        cityArrayList = new ArrayList<>();
+        cityArrayAdapter = new CityArrayAdapter(this, cityArrayList);
+        cityListView.setAdapter(cityArrayAdapter);
+
+        citiesRef.addSnapshotListener((value, error) -> {
+            if (error != null) {
+                Log.e("Firestore", error.toString());
+            }
+            if (value != null) {
+                cityArrayList.clear();
+                for (QueryDocumentSnapshot snapshot : value) {
+                    String name = snapshot.getString("name");
+                    String province = snapshot.getString("province");
+                    cityArrayList.add(new City(name, province));
+                }
+                cityArrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+        // set listeners
+        addCityButton.setOnClickListener(view -> {
+            CityDialogFragment cityDialogFragment = new CityDialogFragment();
+            cityDialogFragment.show(getSupportFragmentManager(),"Add City");
+        });
+
+        cityListView.setOnItemClickListener((adapterView, view, i, l) -> {
+            City city = cityArrayAdapter.getItem(i);
+            CityDialogFragment cityDialogFragment = CityDialogFragment.newInstance(city);
+            cityDialogFragment.show(getSupportFragmentManager(),"City Details");
+        });
+
+    }
+
+    @Override
+    public void updateCity(City city, String title, String year) {
+        // Since city name is used as document ID, updating name requires deleting old and adding new
+        if (!city.getName().equals(title)) {
+            citiesRef.document(city.getName()).delete();
+        }
+        
+        city.setName(title);
+        city.setProvince(year);
+        
+        citiesRef.document(title).set(city);
+    }
+
+    @Override
+    public void addCity(City city){
+        citiesRef.document(city.getName()).set(city);
+    }
+
+    @Override
+    public void deleteCity(City city) {
+        citiesRef.document(city.getName()).delete();
+    }
+
+    public void addDummyData(){
+        City m1 = new City("Edmonton", "AB");
+        City m2 = new City("Vancouver", "BC");
+        cityArrayList.add(m1);
+        cityArrayList.add(m2);
+        cityArrayAdapter.notifyDataSetChanged();
+    }
+}
